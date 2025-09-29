13b14164e8d19d5feb43cea0770941c0632ba7d1
Ashwin Kumar <135273362+1NRP@users.noreply.github.com>
2025-09-29 18:51:38 +0530

    k

--- api/MainServer.js
+++ api/MainServer.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env deno run --version v1.9.0
+#!/usr/bin/env deno run --version v1.44.0
 
 // Vercel-Deno automatically adds the "--allow-all" flag to the "run" command, 
 // which can't be used with other permission flags from Deno version 2.0 onwards.
